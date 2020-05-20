c74b73982b13008026574d0488dc671f04a79e3e
Tom Belote <thomas.belote@braze.com>
2020-05-20 19:17:00 -0400

    Trying out the .framework and some more dependency options

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.2
 
 import PackageDescription
 
@@ -6,16 +6,26 @@ let package = Package(
     name: "AppboyKit",
     platforms: [
         .macOS(.v10_10),
-        .iOS(.v8),
+        .iOS(.v9),
         .tvOS(.v9)
     ],
     products: [
-        .library(name: "AppboyKit", targets: ["AppboyKit"]),
+        .library(name: "AppboyKit", targets: ["AppboyKit", "AppboyKitSDK"]),
     ],
     dependencies: [
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")
     ],
     targets: [
         .systemLibrary(name: "AppboyKit", path: "AppboyKit"),
+        .target(name: "AppboyKitSDK", path: "AppboyKit",
+			sources: ["."],
+			publicHeadersPath: "headers/AppboyKitLibrary",
+			linkerSettings: [
+				.linkedLibrary("z"),
+				.linkedFramework("SystemConfiguration", .when(platforms: [.iOS])),
+				.linkedFramework("QuartzCore", .when(platforms: [.iOS])),
+				.linkedFramework("CoreText", .when(platforms: [.iOS])),
+				.linkedFramework("WebKit", .when(platforms: [.iOS])),
+			])
     ]
 )
