a9ca3c72f79edcd8ba0796272974e2f86b8e3ca8
Tom Belote <thomas.belote@braze.com>
2020-05-21 06:37:26 -0400

    another pass at making a package library

--- Package.swift
+++ Package.swift
@@ -11,22 +11,22 @@ let package = Package(
     ],
     products: [
         .library(name: "AppboyKit", targets: ["AppboyKit"]),
-        //.executable(name: "AppboyKitSDK", targets: ["AppboyKitSDK"])
+    	.library(name: "AppboyKitSDK", targets: ["AppboyKitSDK"])
     ],
     dependencies: [
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")
     ],
     targets: [
         .systemLibrary(name: "AppboyKit", path: "AppboyKit"),
-        //.target(name: "AppboyKitSDK", path: "AppboyKit",
-//			sources: ["."],
-//			publicHeadersPath: "headers/AppboyKitLibrary",
-//			linkerSettings: [
-//				.linkedLibrary("z"),
-//				.linkedFramework("SystemConfiguration", .when(platforms: [.iOS])),
-//				.linkedFramework("QuartzCore", .when(platforms: [.iOS])),
-//				.linkedFramework("CoreText", .when(platforms: [.iOS])),
-//				.linkedFramework("WebKit", .when(platforms: [.iOS])),
-//			]) // must have a main.swift for this to work.
+        .target(name: "AppboyKitSDK", path: "AppboyKit",
+		sources: ["."],
+		publicHeadersPath: "headers/AppboyKitLibrary",
+		linkerSettings: [
+			.linkedLibrary("z"),
+			.linkedFramework("SystemConfiguration", .when(platforms: [.iOS])),
+			.linkedFramework("QuartzCore", .when(platforms: [.iOS])),
+			.linkedFramework("CoreText", .when(platforms: [.iOS])),
+			.linkedFramework("WebKit", .when(platforms: [.iOS])),
+		]) // must have a main.swift for this to work.
     ]
 )
