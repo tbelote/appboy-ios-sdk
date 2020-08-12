1c0ae953b8a1ed6536fb0add78820c53ea9cea59
Tom Belote <thomas.belote@braze.com>
2020-08-12 13:42:08 -0400

    trying again with xcode 12

--- Package.swift
+++ Package.swift
@@ -10,16 +10,25 @@ let package = Package(
         .tvOS(.v9)
     ],
     products: [
-        .library(name: "AppboyKit", targets: ["AppboyKit"]),
+        .library(name: "AppboyKit-core", targets: ["AppboyKit-core"]),
+        //.library(name: "AppboyKit", targets: ["AppboyKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.1.0")
+        .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.8.2")
     ],
     targets: [
-	 .binaryTarget(
-            name: "AppboyKit",
-            url: "https://github.com/Appboy/appboy-ios-sdk/releases/download/3.23.0/Appboy_iOS_SDK.framework.zip",
-            checksum: "99af66310857000d0acce73e3729136afcf88135e9ce6e0beee773a402c81e96"
-        )
+//    	.binaryTarget(
+//            name: "AppboyKit",
+//            url: "https://github.com/Appboy/appboy-ios-sdk/releases/download/3.27.0-beta1/Appboy_iOS_SDK.framework.zip",
+//            checksum: ""
+//      ),
+      .binaryTarget(
+            name: "AppboyKit-core",
+            url: "https://github.com/Appboy/appboy-ios-sdk/releases/download/3.27.0-beta1/Appboy_iOS_SDK_core.framework.zip",
+            checksum: "a4d12d16684d423d008e3c86e05d8db044bc5d65d374ff0632ea2636fd9146d5"
+            //Must be run from same folder as this file
+            //swift package compute-checksum ~/Downloads/Appboy_iOS_SDK_core.framework.zip
+      )
+
     ]
 )
