0c8f284eb25911e23706d18fc0d1763a006cd262
Tom Belote <thomas.belote@braze.com>
2020-08-17 10:37:00 -0400

    First draft at adding the UI sources

--- Package.swift
+++ Package.swift
@@ -10,8 +10,8 @@ let package = Package(
         .tvOS(.v9)
     ],
     products: [
-        //.library(name: "Appboy_iOS_SDK", targets: ["Appboy_iOS_SDK"]),
         .library(name: "Appboy_iOS_SDK", targets: ["Appboy_iOS_SDK"]),
+        .library(name: "AppboyUI", targets: ["AppboyUI"])
     ],
     dependencies: [
         .package(url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.8.2")
@@ -25,13 +25,6 @@ let package = Package(
             //swift package compute-checksum ~/Downloads/Appboy_iOS_SDK.framework.zip
 
        ),
-//      .binaryTarget(
-//            name: "Appboy_iOS_SDK",
-//            url: "https://github.com/Appboy/appboy-ios-sdk/releases/download/3.27.0-beta1/Appboy_iOS_SDK_core.framework.zip",
-//            checksum: "a4d12d16684d423d008e3c86e05d8db044bc5d65d374ff0632ea2636fd9146d5"
-//            //Must be run from same folder as this file
-//            //swift package compute-checksum ~/Downloads/Appboy_iOS_SDK_core.framework.zip
-//      )
-
+       .target(name: "AppboyUI", sources: ["AppboyUI"])
     ]
 )
