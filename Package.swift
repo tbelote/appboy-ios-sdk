b3564357c492bf250bb4fa9b1570ecc53e2597fc
Tom Belote <thomas.belote@braze.com>
2020-08-19 13:49:41 -0400

    path errors

--- Package.swift
+++ Package.swift
@@ -34,7 +34,7 @@ let package = Package(
 ]
       ),
             .target(name: "AppboyContentCards",
-              path: "AppboyUI/ABKContentCards",
+              path: "ABKContentCards",
               sources: [
 "ViewControllers/Cells/ABKClassicImageContentCardCell.m",
 "ViewControllers/Cells/ABKClassicContentCardCell.m",
@@ -45,12 +45,12 @@ let package = Package(
 "ViewControllers/ABKContentCardsWebViewController.m",
 "ViewControllers/ABKContentCardsViewController.m"
 ],
-resources: [.process("ABKContentCards/Resources"),
-.process("images/appboy_cc_icon_pinned.png"),
-.process("images/appboy_cc_noimage_lrg.png"),
-.process("images/appboy_cc_icon_pinned@2x.png"),
-.process("images/appboy_cc_noimage_lrg@2x.png"),
-.process("images/appboy_cc_icon_pinned@3x.png")
+resources: [.process("Resources"),
+.process("Resources/images/appboy_cc_icon_pinned.png"),
+.process("Resources/images/appboy_cc_noimage_lrg.png"),
+.process("Resources/images/appboy_cc_icon_pinned@2x.png"),
+.process("Resources/images/appboy_cc_noimage_lrg@2x.png"),
+.process("Resources/images/appboy_cc_icon_pinned@3x.png")
        
            ]
       ),
@@ -66,7 +66,7 @@ resources: [.process("ABKContentCards/Resources"),
 "ViewControllers/ABKNewsFeedViewController.m"
 ],
 resources: [
-           .process("ABKNewsFeed/Resources"),
+           .process("Resources"),
 .process("Resources/images/img-noimage-lrg.png"),
 .process("Resources/images/img-noimage-lrg@2x.png"),
 .process("Resources/images/Icons_Unread.png"),
@@ -94,7 +94,7 @@ resources: [
 "ViewControllers/ABKInAppMessageHTMLBaseViewController.m"
 ],
 resources: [
-           .process("ABKInAppMessage/Resources"),
+           .process("Resources"),
 .process("Resources/com_appboy_inapp_close_icon@3x.png"),
 .process("Resources/FontAwesome.otf"),
 .process("Resources/com_appboy_inapp_close_icon.png"),
